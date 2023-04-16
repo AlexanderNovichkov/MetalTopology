ea6dcc17ee1a7904ef14fdc51331aaa2cd18c4f3
Alexander Novichkov <a.novichkov01@gmail.com>
2023-04-16 15:07:45 +0300

    Initial Commit

---
+++ MetalTopology/MetalTopology.h
@@ -0,0 +1,18 @@
+//
+//  MetalTopology.h
+//  MetalTopology
+//
+//  Created by Александр Новичков on 16.04.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MetalTopology.
+FOUNDATION_EXPORT double MetalTopologyVersionNumber;
+
+//! Project version string for MetalTopology.
+FOUNDATION_EXPORT const unsigned char MetalTopologyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MetalTopology/PublicHeader.h>
+
+
